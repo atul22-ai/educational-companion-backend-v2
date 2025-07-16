61ee7b39f45ba4aef4d77d37a55323060dd985c8
Atul Raman <atulraman22@gmail.com>
2025-07-16 12:26:20 +0530

    Added /api prefix to routes

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/", routes);
+app.use("/api", routes);
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("🎉 Connected to MongoDB!"))
